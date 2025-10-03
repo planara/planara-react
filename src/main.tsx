15b97abf3f0f85f98dfe6acfb2dc459c363949a2
a1unade <tags0956@gmail.com>
2025-10-04 01:11:14 +0300

    [add] editor-renderer

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+// Styles
+import './index.css'
+// Components
+import EditorRenderer from "./components/renderers/editor-renderer.tsx";
+
+createRoot(document.getElementById('root')!).render(
+  <EditorRenderer />
+)
