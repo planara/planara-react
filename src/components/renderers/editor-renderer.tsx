57b95717673c6f5239d302c561f89e1e88ea8820
a1unade <tags0956@gmail.com>
2025-10-31 01:23:28 +0300

    [add] add/delete base figures

--- src/components/renderers/editor-renderer.tsx
+++ src/components/renderers/editor-renderer.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
-import cubeObj from '../../assets/cube.obj?raw';
-import { createAppHub, EditorHub, ObjLoader } from '@planara/core';
-import { DisplayMode, ToolType } from '@planara/types';
+// import cubeObj from '../../assets/cube.obj?raw';
+import { createAppHub, EditorHub } from '@planara/core';
+import { DisplayMode, FigureType, SceneMode, SelectMode, ToolType } from '@planara/types';
 
 const EditorRenderer: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -39,15 +39,15 @@ const EditorRenderer: React.FC = () => {
     };
   }, []);
 
-  useEffect(() => {
-    if (!rendererRef.current) return;
-
-    const renderer = rendererRef.current!;
-    const loader = new ObjLoader();
-
-    const figure = loader.load(cubeObj);
-    renderer.addFigure(figure);
-  }, []);
+  // useEffect(() => {
+  //   if (!rendererRef.current) return;
+  //
+  //   const renderer = rendererRef.current!;
+  //   const loader = new ObjLoader();
+  //
+  //   const figure = loader.load(cubeObj);
+  //   renderer.addFigure(figure);
+  // }, []);
 
   return (
     <>
@@ -60,6 +60,32 @@ const EditorRenderer: React.FC = () => {
       </button>
       <button onClick={() => rendererRef.current?.setToolMode(ToolType.Scale)}>Scale</button>
       <button onClick={() => rendererRef.current?.setToolMode(ToolType.Rotate)}>Rotate</button>
+      <button onClick={() => rendererRef.current?.setSelectMode(SelectMode.Mesh)}>Mesh</button>
+      <button onClick={() => rendererRef.current?.setSelectMode(SelectMode.Edge)}>Edge</button>
+      <button onClick={() => rendererRef.current?.addFigure(SceneMode.AddFigure, FigureType.Cube)}>
+        Add Cube
+      </button>
+      <button onClick={() => rendererRef.current?.addFigure(SceneMode.AddFigure, FigureType.Plane)}>
+        Add Plane
+      </button>
+      <button
+        onClick={() => rendererRef.current?.addFigure(SceneMode.AddFigure, FigureType.Cylinder)}
+      >
+        Add Cylinder
+      </button>
+      <button
+        onClick={() => rendererRef.current?.addFigure(SceneMode.AddFigure, FigureType.Sphere)}
+      >
+        Add Sphere
+      </button>
+      <button
+        onClick={() => rendererRef.current?.addFigure(SceneMode.AddFigure, FigureType.UVSphere)}
+      >
+        Add UVSphere
+      </button>
+      <button onClick={() => rendererRef.current?.setSceneMode(SceneMode.DeleteFigure)}>
+        Delete
+      </button>
       <div className="editor-renderer__container">
         <canvas ref={canvasRef} height={1000} width={1000} />
       </div>
